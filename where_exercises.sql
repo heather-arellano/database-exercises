a63f64d39aa70c76f7c577a9773622e83effece2
heather arellano <hma.485@icloud.com>
2018-05-23 14:31:05 -0500

    Commit where_exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,11 +2,11 @@ USE employees;
 
 SELECT  first_name
 FROM employees
-WHERE first_name IN ('Irena','Vidya', 'Maya');
+WHERE (first_name  = 'Irena' or first_name  = 'Vidya' or first_name  = 'Maya') 
 
 SELECT last_name
 FROM employees
-WHERE last_name LIKE 'E%';
+WHERE last_name LIKE 'E%' or last_name LIKE '%e';
 
 
 SELECT *
