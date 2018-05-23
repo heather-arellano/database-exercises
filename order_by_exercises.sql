6dc4a47608a2c9089903296c5bd818defb5585bc
heather arellano <hma.485@icloud.com>
2018-05-23 16:08:35 -0500

    Commit where_exercise

---
+++ order_by_exercises.sql
@@ -0,0 +1,20 @@
+USE employees;
+
+
+
+SELECT  first_name, last_name
+FROM employees
+WHERE (first_name  = 'Irena' or first_name  = 'Vidya' or first_name  = 'Maya') AND order by first_name ASC;
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%e';
+
+
+SELECT *
+FROM employees
+WHERE hire_date like '%199%' and birth_date LIKE '%12-25';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND  last_name NOT like 'qu%';
