d520d01be36cd308aa0ab50899de504c782b3132
heather arellano <hma.485@icloud.com>
2018-05-23 14:39:09 -0500

    Commit where_exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -6,17 +6,13 @@ WHERE (first_name  = 'Irena' or first_name  = 'Vidya' or first_name  = 'Maya') A
 
 SELECT last_name
 FROM employees
-WHERE last_name LIKE 'E%' or last_name LIKE '%e';
+WHERE last_name LIKE 'E%' AND last_name LIKE '%e';
 
 
 SELECT *
 FROM employees
-WHERE hire_date like '%199%';
-
-SELECT *
-FROM employees
-WHERE birth_date LIKE '%12-25';
+WHERE hire_date like '%199%' and birth_date LIKE '%12-25';
 
 SELECT last_name
 FROM employees
-WHERE last_name LIKE '%q%';
+WHERE last_name LIKE '%q%' AND  last_name NOT like 'qu%';
