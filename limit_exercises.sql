56c56ed44996f0ebe8a6c80d55a8f8ae0e5ffc54
heather arellano <hma.485@icloud.com>
2018-05-23 16:48:21 -0500

    Commit limit_exercise

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,5 +1,12 @@
 use employees;
 
+SELECT DISTINCT last_name
+FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
 SELECT *
-from quotes
-limit 2 offset 0;
+FROM employees
+WHERE hire_date like '%199%' and birth_date LIKE '%12-25'
+ORDER BY birth_date  , hire_date DESC
+LIMIT 5 OFFSET 45;
