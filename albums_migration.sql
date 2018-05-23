61da9adb1df87fa11ea89de8b780dcd40e9316d2
heather arellano <hma.485@icloud.com>
2018-05-23 09:30:21 -0500

    Commit select_exercise

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE IF NOT EXISTS albums (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+artist VARCHAR(100) NOT NULL,
+record_name VARCHAR(150),
+release_date SMALLINT UNSIGNED,
+sales FLOAT(6, 2),
+genre VARCHAR(50),
+PRIMARY KEY (id)
+);
